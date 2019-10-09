27692845ab52e7fd1f24b6d1c8efebc8f1245fe2
fullyalive <wogud1202@gmail.com>
2019-10-09 16:44:31 +0900

    UserApp | 유저어드민 + 커스텀어드민 붙이기

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from . import models
 
 
-@admin.register(models.User)
-class CustomUserAdmin(admin.ModelAdmin):
-    pass
+@admin.register(models.User)  # admin.site.register(models.User, CustomUserAdmin) 과 동일
+class CustomUserAdmin(UserAdmin):
+
+    """ Custom User Admin """
+
+    list_display = ("superhost", "username", "email", "language", "currency")
+    list_filter = ("superhost", "gender", "currency", "language")
+    fieldsets = (
+        (
+            "사용자 기본 정보",
+            {
+                "fields": (
+                    "superhost",
+                    "avatar",
+                    "gender",
+                    "bio",
+                    "birthdate",
+                    "language",
+                    "currency",
+                )
+            },
+        ),
+    ) + UserAdmin.fieldsets
