7e04562129eba1b7dfcee881bd856643efd6ffe5
fullyalive <wogud1202@gmail.com>
2019-10-09 15:52:12 +0900

    UserApp | gender, avatar 필드 추가
    pipenv install Pillow

--- users/models.py
+++ users/models.py
@@ -4,4 +4,20 @@ from django.db import models
 
 class User(AbstractUser):
 
-    bio = models.TextField(default="")
+    """ Custom User Model """
+
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OTHER = "other"
+
+    GENDER_CHOICES = ((GENDER_MALE, "남성"), (GENDER_FEMALE, "여성"), (GENDER_OTHER, "기타"))
+
+    avatar = models.ImageField(null=True, blank=True)
+    gender = models.CharField(
+        default=GENDER_MALE,
+        choices=GENDER_CHOICES,
+        max_length=10,
+        null=True,
+        blank=True,
+    )
+    bio = models.TextField(default="", blank=True)
