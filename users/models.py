2ac6ec789103eff9d35ce622b2be1efea1ce2630
fullyalive <wogud1202@gmail.com>
2019-10-09 16:14:05 +0900

    UserApp | birthdate, language, currency, superhost 필드 추가

--- users/models.py
+++ users/models.py
@@ -9,8 +9,15 @@ class User(AbstractUser):
     GENDER_MALE = "male"
     GENDER_FEMALE = "female"
     GENDER_OTHER = "other"
-
     GENDER_CHOICES = ((GENDER_MALE, "남성"), (GENDER_FEMALE, "여성"), (GENDER_OTHER, "기타"))
+
+    LANGUAGE_ENGLISH = "en"
+    LANGUAGE_KOREAN = "kr"
+    LANGUAGE_CHOICES = ((LANGUAGE_ENGLISH, "english"), (LANGUAGE_KOREAN, "한국어"))
+
+    CURRENCY_USD = "usd"
+    CURRENCY_KRW = "krw"
+    CURRENCY_CHOICES = ((CURRENCY_USD, "USD"), (CURRENCY_KRW, "KRW"))
 
     avatar = models.ImageField(null=True, blank=True)
     gender = models.CharField(
@@ -21,3 +28,19 @@ class User(AbstractUser):
         blank=True,
     )
     bio = models.TextField(default="", blank=True)
+    birthdate = models.DateField(null=True)
+    language = models.CharField(
+        default=LANGUAGE_KOREAN,
+        choices=LANGUAGE_CHOICES,
+        max_length=2,
+        null=True,
+        blank=True,
+    )
+    currency = models.CharField(
+        default=CURRENCY_KRW,
+        choices=CURRENCY_CHOICES,
+        max_length=3,
+        null=True,
+        blank=True,
+    )
+    superhost = models.BooleanField(default=False)
