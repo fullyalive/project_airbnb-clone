02189b6b635060d4d43afbe87bdbf330e3a68b7f
fullyalive <wogud1202@gmail.com>
2019-10-10 10:58:22 +0900

    RoomApp | TimeStamped Model - updated, created 상속 사용

--- users/models.py
+++ users/models.py
@@ -19,28 +19,16 @@ class User(AbstractUser):
     CURRENCY_KRW = "krw"
     CURRENCY_CHOICES = ((CURRENCY_USD, "USD"), (CURRENCY_KRW, "KRW"))
 
-    avatar = models.ImageField(null=True, blank=True)
+    avatar = models.ImageField(blank=True)
     gender = models.CharField(
-        default=GENDER_MALE,
-        choices=GENDER_CHOICES,
-        max_length=10,
-        null=True,
-        blank=True,
+        default=GENDER_MALE, choices=GENDER_CHOICES, max_length=10, blank=True
     )
-    bio = models.TextField(default="", blank=True)
-    birthdate = models.DateField(null=True)
+    bio = models.TextField(blank=True)
+    birthdate = models.DateField(blank=True, null=True)
     language = models.CharField(
-        default=LANGUAGE_KOREAN,
-        choices=LANGUAGE_CHOICES,
-        max_length=2,
-        null=True,
-        blank=True,
+        default=LANGUAGE_KOREAN, choices=LANGUAGE_CHOICES, max_length=2, blank=True
     )
     currency = models.CharField(
-        default=CURRENCY_KRW,
-        choices=CURRENCY_CHOICES,
-        max_length=3,
-        null=True,
-        blank=True,
+        default=CURRENCY_KRW, choices=CURRENCY_CHOICES, max_length=3, blank=True
     )
     superhost = models.BooleanField(default=False)
