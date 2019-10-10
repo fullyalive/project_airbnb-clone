da12227a1de1e3a8f2377a25551130ec38ae2219
fullyalive <wogud1202@gmail.com>
2019-10-10 23:15:35 +0900

    RoomApp | Facility, Amenity, RoomType, HouseRule class 추가

--- rooms/admin.py
+++ rooms/admin.py
@@ -2,7 +2,25 @@ from django.contrib import admin
 from . import models
 
 
+@admin.register(models.RoomType, models.Facility, models.Amenity, models.HouseRule)
+class ItemAdmin(admin.ModelAdmin):
+
+    """ Item Admin Definition """
+
+    pass
+
+
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
+
+    """ Item Admin Definition """
+
+    pass
+
+
+@admin.register(models.Photo)
+class PhotoAdmin(admin.ModelAdmin):
+
+    """ Photo Admin Definition """
 
     pass
